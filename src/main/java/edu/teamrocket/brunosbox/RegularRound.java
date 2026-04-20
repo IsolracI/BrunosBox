39f8f1f9133570d7e0578101248134ac78f91fcf
|solrac| <gabrieldiacich@gmail.com>
2026-04-20 14:14:46 +0200

    feat(RegularRound): diseñar clase RegularRound para pasar casos test

---
+++ src/main/java/edu/teamrocket/brunosbox/RegularRound.java
@@ -0,0 +1,42 @@
+package edu.teamrocket.brunosbox;
+
+import java.lang.reflect.Type;
+
+public class RegularRound implements Round{
+    private final String roundScore;
+    private byte redBoxerScore;
+    private byte blueBoxerScore;
+
+    public RegularRound(String roundScore) {
+     this.roundScore = roundScore.replaceAll("\\s", "" );
+     this.parseBoxerRoundScore();
+    }
+
+    private void parseBoxerRoundScore() {
+        String[] score = this.roundScore.split("-", 2);
+        this.redBoxerScore = Byte.parseByte(score[0]);
+        this.blueBoxerScore = Byte.parseByte(score[1]);
+    }
+
+    public String getRoundScore() {
+        return this.roundScore;
+    }
+
+    @Override
+    public byte getBlueBoxerScore() {
+        return this.blueBoxerScore;
+    }
+
+    @Override
+    public byte getRedBoxerScore() {
+        return this.redBoxerScore;
+    }
+
+    @Override
+    public String toString() {
+        return this.redBoxerScore + "-" + this.blueBoxerScore;
+    }
+
+
+}
+
---
+++ src/main/java/edu/teamrocket/brunosbox/Round.java
@@ -0,0 +1,6 @@
+package edu.teamrocket.brunosbox;
+
+public interface Round {
+    byte getRedBoxerScore();
+    byte getBlueBoxerScore();
+}
